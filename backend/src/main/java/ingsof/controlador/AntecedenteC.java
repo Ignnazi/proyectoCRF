7aa6ec9b2ec0d6d5cae546d4f8c32544769137c0
Racky17 <dayestray@gmail.com>
2025-12-17 22:46:08 -0300

    AntecedentesST

--- backend/src/main/java/ingsof/controlador/AntecedenteC.java
+++ backend/src/main/java/ingsof/controlador/AntecedenteC.java
@@ -1,11 +1,19 @@
 package ingsof.controlador;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ingsof.entidad.Antecedente;
 import ingsof.servicio.AntecedenteS;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 @CrossOrigin(origins = "http://localhost:3002")
 @RestController
 @RequestMapping("/api/antecedente")
