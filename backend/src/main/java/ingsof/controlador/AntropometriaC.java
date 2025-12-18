cb986f8ff691b846afb425b36cbc7e7ca1c0198e
William San Martín <102183183+MrHandrol1@users.noreply.github.com>
2025-12-17 23:25:32 -0300

    Modificacion para base de datos
    Modifique algunos parametros para poder ejecutar la base de datos en mi sistema

--- backend/src/main/java/ingsof/controlador/AntropometriaC.java
+++ backend/src/main/java/ingsof/controlador/AntropometriaC.java
@@ -1,12 +1,20 @@
 package ingsof.controlador;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import ingsof.entidad.Antropometria;
 import ingsof.servicio.AntropometriaS;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-@CrossOrigin(origins = "http://localhost:3002")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/antropometria")
 public class AntropometriaC {
--- backend/src/main/java/ingsof/controlador/SociodemoC.java
+++ backend/src/main/java/ingsof/controlador/SociodemoC.java
@@ -1,13 +1,22 @@
 package ingsof.controlador;
 
-import ingsof.entidad.Sociodemo;
-import ingsof.servicio.SociodemoS;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "http://localhost:3002")
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ingsof.entidad.Sociodemo;
+import ingsof.servicio.SociodemoS;
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/sociodemo")
 public class SociodemoC {
